8fcb93e076c4c689b0e3061b2c9de3640e151c14
AbdallahGado <abdallahgado22@gmail.com>
2026-01-05 16:26:38 +0200

    1.3

--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -5,9 +5,13 @@ import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
-
 export const ConvexClintProvider = ({ children }: { children: ReactNode }) => {
+  if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+    return <>{children}</>;
+  }
+
+  const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+
   return (
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
